d851aadad1c48aaa90afcde39d9bffc3abf7d3dc
nilin <by.diuqi@qq.com>
2023-08-12 22:44:18 +0800

    tijiao

---
+++ config.js
@@ -0,0 +1,34 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '逆鳞监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2260093-807e2170b5e92e79918ff11c',
+    'u2260093-cd1bda17ac489a31952958be',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '首页',
+      url: 'https://nilin.cc/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.nilin.cc/'
+    },
+  ],
+};
